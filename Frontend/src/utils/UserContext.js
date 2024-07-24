1e6e3de50f7f7aafb48d9373c750a816913c5142
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-07-25 00:02:31 +0530

    Delete Frontend/src directory

--- Frontend/src/utils/UserContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const UserContext = createContext({
-  loggedInUser: "",
-  setLoggedInUser: () => {}, // Provide a default function
-});
-
-export default UserContext;
