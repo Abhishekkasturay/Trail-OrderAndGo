3a2c32d8656be5f28c29647e56524653210ec088
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-07-24 22:19:29 +0530

    Add files via upload

---
+++ Frontend/src/utils/constants.js
@@ -0,0 +1,8 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Swiggy_logo.svg/2560px-Swiggy_logo.svg.png";
+
+export const MENU_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.4484257&lng=78.5684594&restaurantId=";
