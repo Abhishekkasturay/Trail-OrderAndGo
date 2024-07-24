d6e54584fc3e40ab3cc219bbc11b9d2237856eb7
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-07-24 23:00:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Allow CORS from specific origins
 const corsOptions = {
-  origin: "https://orderandgo.onrender.com", 
+  origin: "https://orderandgo.onrender.com", // Replace with your actual frontend domain
   optionsSuccessStatus: 200,
 };
 
@@ -34,12 +34,11 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/trains", trainRoutes);
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../client/dist')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
