841a4901a12e5f57154fade9f35e818f2ad212f1
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-09-29 10:52:44 +0530

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],  // Add the Roboto font here
+      },
+    },
   },
   plugins: [],
 };
